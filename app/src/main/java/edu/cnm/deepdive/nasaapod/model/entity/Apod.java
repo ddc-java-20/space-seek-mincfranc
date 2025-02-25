86b9a7d2fe13a61082cae39df54d88a000cafab9
Minerva Franco <mfranco35@cnm.edu>
2025-02-25 10:33:54 -0700

    Completed Apod entity class.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/model/entity/Apod.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/entity/Apod.java
@@ -1,17 +1,27 @@
 package edu.cnm.deepdive.nasaapod.model.entity;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import java.net.URL;
 import java.time.LocalDate;
 
+@Entity(tableName = "apod")
 public class Apod {
 
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "apod_id")
+  private long Id;
+
+  @ColumnInfo(index = true, collate = ColumnInfo.NOCASE)
   @Expose
   @NonNull
   private String title;
 
+  @ColumnInfo(collate = ColumnInfo.NOCASE)
   @Expose
   @SerializedName("explanation")
   private String description;
@@ -23,82 +33,75 @@ public class Apod {
   @Expose
   private String copyright;
 
+  @ColumnInfo(name = "media_type", index = true)
   @Expose
   @SerializedName("media_type")
   @NonNull
   private MediaType mediaType;
 
+  @ColumnInfo(name = "low_def_url")
   @Expose
   @SerializedName("url")
+  @NonNull
   private URL lowDefUrl;
 
+  @ColumnInfo(name = "high_def_url")
   @Expose
   @SerializedName("hdurl")
-  private URL highDefdurl;
+  private URL highDefdUrl;
+
+  public long getId() {return Id;}
+
+  public void setId(long id) {Id = id;}
 
   @NonNull
-  public String getTitle() {
-    return title;
-  }
+  public String getTitle() {return title;}
 
-  public void setTitle(@NonNull String title) {
-    this.title = title;
-  }
+  public void setTitle(@NonNull String title) {this.title = title;}
 
-  public String getDescription() {
-    return description;
-  }
+  public String getDescription() {return description;}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+  public void setDescription(String description) {this.description = description;}
 
   @NonNull
-  public LocalDate getDate() {
-    return date;
-  }
+  public LocalDate getDate() {return date;}
 
-  public void setDate(@NonNull LocalDate date) {
-    this.date = date;
-  }
+  public void setDate(@NonNull LocalDate date) {this.date = date;}
 
-  public String getCopyright() {
-    return copyright;
-  }
+  public String getCopyright() {return copyright;}
 
-  public void setCopyright(String copyright) {
-    this.copyright = copyright;
-  }
+  public void setCopyright(String copyright) {this.copyright = copyright;}
 
   @NonNull
-  public MediaType getMediaType() {
-    return mediaType;
-  }
+  public MediaType getMediaType() {return mediaType;}
 
-  public void setMediaType(@NonNull MediaType mediaType) {
-    this.mediaType = mediaType;
-  }
+  public void setMediaType(@NonNull MediaType mediaType) {this.mediaType = mediaType;}
 
-  public URL getLowDefUrl() {
-    return lowDefUrl;
-  }
+  @NonNull
+  public URL getLowDefUrl() {return lowDefUrl;}
 
-  public void setLowDefUrl(URL lowDefUrl) {
-    this.lowDefUrl = lowDefUrl;
-  }
+  public void setLowDefUrl(@NonNull URL lowDefUrl) {this.lowDefUrl = lowDefUrl;}
 
-  public URL getHighDefdurl() {
-    return highDefdurl;
-  }
+  public URL getHighDefdUrl() {return highDefdUrl;}
 
-  public void setHighDefdurl(URL highDefdurl) {
-    this.highDefdurl = highDefdurl;
-  }
+  public void setHighDefUrl(URL highDefdUrl) {this.highDefdUrl = highDefdUrl;}
 
   public enum MediaType {
     @SerializedName("Image")
-    IMAGE,
+    IMAGE(true),
     @SerializedName("Video")
-    VIDEO
+    VIDEO(false);
+
+    private final boolean downloadable;
+
+    MediaType(boolean downloadable) {
+      this.downloadable = downloadable;
+    }
+
+    //this is a generated getter for downloadable
+    public boolean isDownloadable() {
+      return downloadable;
+    }
+
   }
 }
